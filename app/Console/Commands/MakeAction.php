1310b40052f771c29875af9d0e2037e975a0c13e
Nielen Seldte <129792303+nielenseldte@users.noreply.github.com>
2025-09-29 11:07:30 +0200

    wip on artisan command for action code generation

--- app/Console/Commands/MakeAction.php
+++ app/Console/Commands/MakeAction.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands;
 
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 
 class MakeAction extends Command implements PromptsForMissingInput
@@ -23,6 +25,78 @@ class MakeAction extends Command implements PromptsForMissingInput
     {name : The name of your action. eg. ShowPostAction} 
     {model? : the eloquent model you want to link to the action} 
     {--logable : Whether to make an optional logResults() method on the Action class}';
+
+    protected function promptForMissingArgumentsUsing(): array
+    {
+        return [
+            'name' => ['What should you Action be called? Think about it logically, what does this class do?', 'E.g. ShowPostAction'],
+        ];
+    }
+
+    protected function createActionInterface()
+    {
+        $directory = app_path('Actions/Contracts');
+        $filePath = $directory . '/ActionInterface.php';
+        $stubPath = base_path('stubs/action.interface.stub');
+
+        if (!File::exists($directory)) {
+            File::makeDirectory($directory, 0755, true);
+        }
+
+        if (!File::exists($filePath)) {
+            $content = File::get($stubPath);
+            File::put($filePath, $content);
+        }
+    }
+
+    protected function makeStandardAction(string $name, bool $logable)
+    {
+        $directory = app_path('Actions');
+        $filePath = $directory . "/$name.php";
+        $logable ?  $stubPath = base_path('stubs/action.class.standard.stub') : $stubPath = base_path('stubs/action.class.standard.logable.stub');
+
+        $stub = File::get($stubPath);
+
+        $replacements = [
+            '{{ namespace }}' => "App\Actions",
+            '{{ class }}' => $name,
+        ];
+
+        $content = str_replace(array_keys($replacements), array_values($replacements), $stub);
+
+        if (!File::exists($directory)) {
+            File::makeDirectory($directory, 0755, true);
+        }
+
+        if (!File::exists($filePath)) {
+            File::put($filePath, $content);
+        }
+    }
+
+    protected function makeModelAction(string $name, string $modelName, bool $logable)
+    {
+        $directory = app_path("Actions/$modelName");
+        $filePath = $directory . "/$name.php";
+        $logable ?  $stubPath = base_path('stubs/action.class.model.stub') : $stubPath = base_path('stubs/action.class.model.logable.stub');
+
+        $stub = File::get($stubPath);
+
+        $replacements = [
+            '{{ namespace }}' => 'App\Actions' .'\\'. $modelName,
+            '{{ class }}' => $name,
+            '{{ model }}' => $modelName
+        ];
+
+        $content = str_replace(array_keys($replacements), array_values($replacements), $stub);
+
+        if (!File::exists($directory)) {
+            File::makeDirectory($directory, 0755, true);
+        }
+
+        if (!File::exists($filePath)) {
+            File::put($filePath, $content);
+        }
+    }
 
     /**
      * Execute the console command.
@@ -56,13 +130,17 @@ class MakeAction extends Command implements PromptsForMissingInput
             $makeLogMethod = $logChoice === "Yes";
         }
 
-        $this->info("Name: {$name}, Model: {$model}, Make Log method?: {$makeLogMethod}");
+        $this->createActionInterface();
+
+        if (!$model) {
+            $this->makeStandardAction($name, $makeLogMethod);
+        } else {
+            $modelName = Str::before($model, '.');
+            $this->makeModelAction($name, $modelName, $makeLogMethod);
+        }
+
+        $this->info("DONE: Created an Action class '$name'");
     }
 
-    protected function promptForMissingArgumentsUsing(): array
-    {
-        return [
-            'name' => ['What should you Action be called? Think about it logically, what does this class do?', 'E.g. ShowPostAction'],   
-        ];
-    }
+    
 }
