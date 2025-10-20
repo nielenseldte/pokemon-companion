29328dcdfa78933a37994b271adaac804a437668
Nielen Seldte <129792303+nielenseldte@users.noreply.github.com>
2025-10-20 11:55:21 +0200

    Add scrollable team container component
    Introduces ScrollableTeamContainer.vue for horizontal scrolling of team cards with navigation controls. Updates Teams/Index.vue to use the new component for displaying teams.

---
+++ resources/js/Components/Teams/ScrollableTeamContainer.vue
@@ -0,0 +1,66 @@
+<script setup>
+import { ref } from 'vue';
+import NavigationButton from '../Buttons/NavigationButton.vue';
+const props = defineProps({
+    teamName: {
+        type: String,
+        required: true
+    }
+})
+const scrollContainer = ref(null);
+let scrollInterval = null;
+
+function scrollLeft() {
+    scrollInterval = setInterval(() => {
+        scrollContainer.value.scrollBy({
+            left: -50,
+            behavior: 'auto'
+        });
+    }, 20);
+}
+
+function scrollRight() {
+    scrollInterval = setInterval(() => {
+        scrollContainer.value.scrollBy({
+            left: 50,
+            behavior: 'auto'
+        });
+    }, 20);
+}
+
+function stopScroll() {
+    if (scrollInterval) {
+        clearInterval(scrollInterval);
+        scrollInterval = null;
+    }
+}
+</script>
+<template>
+    <div class="border-8 bg-purple border-yellow">
+        <div class="flex items-center justify-between mt-2 px-3">
+            <p class="text-xl">{{ teamName }}</p>
+            <NavigationButton href="#">View Details</NavigationButton>
+        </div>
+        <div ref="scrollContainer" class="flex items-center space-x-3 p-3 scrollable-hidden pb-1">
+            <slot />
+        </div>
+        <div class="flex items-center justify-between">
+            <p class="cursor-pointer p-3 hover:text-pink" @mouseenter="scrollLeft" @mouseleave="stopScroll">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-move-left-icon lucide-move-left  transition-colors duration-150">
+                    <path d="M6 8L2 12L6 16" />
+                    <path d="M2 12H22" />
+                </svg>
+            </p>
+            <p class="cursor-pointer p-3 hover:text-pink" @mouseenter="scrollRight" @mouseleave="stopScroll">
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-move-right-icon lucide-move-right transition-colors duration-150">
+                    <path d="M18 8L22 12L18 16" />
+                    <path d="M2 12H22" />
+                </svg>
+            </p>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Teams/Index.vue
+++ resources/js/Pages/Teams/Index.vue
@@ -1,9 +1,48 @@
 <script setup>
+import CardContainer from '../../Components/Cards/CardContainer.vue';
+import ScrollableTeamContainer from '../../Components/Teams/ScrollableTeamContainer.vue';
 
 </script>
 
 <template>
-   
-        <h1>Hello from the my teams page</h1>
-   
+
+        <div class="grid grid-cols-1 gap-5">
+                <ScrollableTeamContainer teamName="A Team">
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url="https://images.pokemontcg.io/ex3/4_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/xy2/4_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/pop5/2_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/det1/1_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/dv1/4_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/dp4/2_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/gym2/3_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/pl4/2_hires.png" />
+                </ScrollableTeamContainer>
+                <ScrollableTeamContainer teamName="B Team">
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url="https://images.pokemontcg.io/ex3/4_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/xy2/4_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/pop5/2_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/det1/1_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/dv1/4_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/dp4/2_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/gym2/3_hires.png" />
+                        <CardContainer class="min-w-1/5" :hover="false" :clickable="false"
+                                image-url=" https://images.pokemontcg.io/pl4/2_hires.png" />
+                </ScrollableTeamContainer>
+        </div>
+
 </template>
